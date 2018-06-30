28a48f7aa18767ea114830e82a4d85a6cc1cf431
shaz88al <shahzad88mails@gmail.com>
2018-06-30 21:06:37 +0530

    Removed unused file and variables

--- src/components/AppMasterContent/index.jsx
+++ src/components/AppMasterContent/index.jsx
@@ -3,18 +3,15 @@ import './index.css'
 import Header from './../Header'
 import Footer from './../Footer'
 import Home from './../Home'
-import { Row, Col } from 'react-bootstrap'
 
 class AppMainContent extends Component {
     render() {
         return (
-            <Row className="zomaaato-lite">
-                <Col>
+            <div>
                 <Header />
                 <Home />
                 <Footer />
-                </Col>
-            </Row>
+            </div>
         );
     }
 }
